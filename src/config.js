c21df23948a427fb23720f6a8c3730371406a3ff
MannanAnxari <mannananxari@gmail.com>
2023-01-17 19:43:24 +0500

    'a'

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    APP_NAME: " - Discount Space",
+    APP_NAME: " - IVY Discount",
     URL: "https://discounts-space.com/",
     SITE_URL: "https://discounts-space.com/",
     API_TOKEN: "10010110101001010"
