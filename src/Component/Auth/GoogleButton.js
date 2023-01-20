ecbc0987d3d98b1700374e755ce5d13ca5c607fd
MannanAnxari <mannananxari@gmail.com>
2023-01-20 12:45:50 +0500

    .

--- src/Component/Auth/GoogleButton.js
+++ src/Component/Auth/GoogleButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { googleLogout } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 
 
 export const GoogleButton = () => {
     return (
         <GoogleLogin
             onSuccess={(credentialResponse) => {
-                console.log(credentialResponse);
+                console.log(jwt_decode(credentialResponse.credential));
             }}
             text={'continue_with'}
             width={'50%'}
